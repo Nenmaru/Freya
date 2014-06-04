e3ca8c1947ade8b7d0fb611cbdb641036da6b600
Nenmaru <nezzi@mail.ru>
2014-06-04 22:54:47 +0300

    Project

---
+++ Freya/src/main/java/ua/kh/kture/web/IndexServlet.java
@@ -0,0 +1,27 @@
+package ua.kh.kture.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 11111111L;
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/index").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
